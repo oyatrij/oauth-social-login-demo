672706a287433589be11eb1363bc30e71ef4a3a1
oyatrij <oyatrij@gmail.com>
2024-04-21 13:38:30 +0900

    create project

---
+++ src/test/java/com/oyatrij/springoauthsociallogin/SpringOauthSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyatrij.springoauthsociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthSocialLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
