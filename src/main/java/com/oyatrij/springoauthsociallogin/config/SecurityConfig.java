58f70cdc47cad54a46cc9e87497e5aea4a956ed0
oyatrij <oyatrij@gmail.com>
2024-04-22 16:30:49 +0900

    oAuth 설정

--- src/main/java/com/oyatrij/springoauthsociallogin/config/SecurityConfig.java
+++ src/main/java/com/oyatrij/springoauthsociallogin/config/SecurityConfig.java
@@ -1,30 +1,51 @@
 package com.oyatrij.springoauthsociallogin.config;
 
+import com.oyatrij.springoauthsociallogin.service.CustomOAuth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
+@Configurable
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration); // 모든 경로에 대해서 CORS 설정을 적용
+
+        return source;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //SpringSecurity 6
-
-        // CORS 설정
-        CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
-        cors.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        cors.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        cors.setAllowCredentials(true);
 
         // CSRF 설정
-        http.csrf(Customizer.withDefaults());
+       http.csrf(Customizer.withDefaults());
+
+        http.oauth2Login(httpSecurityOAuth2LoginConfigurer ->
+            httpSecurityOAuth2LoginConfigurer.loginPage("/login")
+                .userInfoEndpoint(userInfoEndpointConfig ->
+                    userInfoEndpointConfig.userService(customOAuth2UserService)));
 
         return http.build();
     }
